cbacf6974d452940a95d148d2b89e8923dd02edd
ooosuke <oosuke@dissmo.biz>
2023-05-05 01:41:52 +0900

    feat: middlewereを適用してみる

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: false }),
+  );
+  await app.listen(process.env.PORT || 3000, '0.0.0.0'); // 外部ホストからのAPIを受け入れるため0.0.0.0を指定する
 }
 bootstrap();
